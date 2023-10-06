ada43c4b4f2e4f64b80804fac86495b02db8613c
sisonj <98570174+sisonj3@users.noreply.github.com>
2023-10-06 16:32:26 -0700

    Used gameboard module to create button. Added gameController module

--- script.js
+++ script.js
@@ -1,11 +1,62 @@
-// Gameboard object
-let gameBoard = (() => {
-    let board = [['', '', ''],
-                ['', '', ''],
-                ['', '', '']];
-})();
-
 // Player factory
 const playerFactory = (token) => {
-    return {token};
+    const takeTurn = (e) => {
+        e.target.textContent = token;
+    };
+
+    return {takeTurn, token};
 };
+
+// Game controller
+const gameController = (() => {
+    const playerOne = playerFactory('X');
+    const playerTwo = playerFactory('O');
+
+    let isPlayerOneTurn = true;
+    let gameOver = false;
+
+    const takeTurn = (e) => {
+        console.log(e.target.dataset.row + ',' + e.target.dataset.column);
+
+        if(e.target.textContent == '' && !gameOver){
+            if (isPlayerOneTurn){
+                playerOne.takeTurn(e);
+
+                // Check if won
+            } else {
+                playerTwo.takeTurn(e);
+
+                // Check if won
+            }
+
+            isPlayerOneTurn = !isPlayerOneTurn;
+        }
+          
+    }
+
+    return {takeTurn};
+})();
+
+// Gameboard object
+const gameBoard = (() => {
+    // Gameboard div
+    const boardDiv = document.querySelector('.gameboard');
+
+    for(let i = 1; i <= 3; i++){
+        for(let j = 1; j <= 3; j++){
+            const btn = document.createElement('button');
+
+            // Button properties
+            btn.classList.add('cell');
+            btn.dataset.row = i;
+            btn.dataset.column = j;
+            btn.addEventListener('click', gameController.takeTurn);
+
+            // Add button to dom
+            boardDiv.appendChild(btn);
+        }
+    }
+
+    return {boardDiv};
+
+})();
