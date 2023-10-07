72571f507df3d4e1e62306c3bb854f48276cc099
sisonj <98570174+sisonj3@users.noreply.github.com>
2023-10-06 17:08:23 -0700

    Added win conditions

--- script.js
+++ script.js
@@ -14,19 +14,63 @@ const gameController = (() => {
 
     let isPlayerOneTurn = true;
     let gameOver = false;
+    let turnsTaken = 0;
+
+    // Gameboard div
+    const boardDiv = document.querySelector('.gameboard');
+
+    // Gameboard array for checking win condition
+    let boardArr = [['', '' , ''], ['', '' , ''], ['', '' , '']];
+
+    const checkWinner = (token) => {
+        // Check every row and column
+        for(let i = 0; i <= 2; i++){
+            if((boardArr[i][0].textContent == token && boardArr[i][1].textContent == token && boardArr[i][2].textContent == token) ||
+            (boardArr[0][i].textContent == token && boardArr[1][i].textContent == token && boardArr[2][i].textContent == token)) {
+                gameOver = true;
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if(boardArr[1][1].textContent == token && 
+            ((boardArr[0][0].textContent == token && boardArr[2][2].textContent == token) 
+            || (boardArr[2][0].textContent == token && boardArr[0][2].textContent == token))){
+                gameOver = true;
+                return true;
+        }
+
+        // No winner
+        return false;
+    }
 
     const takeTurn = (e) => {
         console.log(e.target.dataset.row + ',' + e.target.dataset.column);
+        console.log('Turns: ' + turnsTaken);
 
         if(e.target.textContent == '' && !gameOver){
             if (isPlayerOneTurn){
+                // Update number of turns taken
+                turnsTaken++;
+
                 playerOne.takeTurn(e);
 
                 // Check if won
+                console.log('One wins: ' + checkWinner(playerOne.token));
             } else {
+                // Update number of turns taken
+                turnsTaken++;
+
                 playerTwo.takeTurn(e);
 
                 // Check if won
+                console.log('Two wins: ' + checkWinner(playerTwo.token));
+            }
+
+            // Game is a draw
+            if(turnsTaken >= 9){
+                gameOver = true;
+                alert('Draw!');
             }
 
             isPlayerOneTurn = !isPlayerOneTurn;
@@ -34,14 +78,7 @@ const gameController = (() => {
           
     }
 
-    return {takeTurn};
-})();
-
-// Gameboard object
-const gameBoard = (() => {
-    // Gameboard div
-    const boardDiv = document.querySelector('.gameboard');
-
+    // Create gameboard
     for(let i = 1; i <= 3; i++){
         for(let j = 1; j <= 3; j++){
             const btn = document.createElement('button');
@@ -50,13 +87,17 @@ const gameBoard = (() => {
             btn.classList.add('cell');
             btn.dataset.row = i;
             btn.dataset.column = j;
-            btn.addEventListener('click', gameController.takeTurn);
+            btn.addEventListener('click', takeTurn);
 
             // Add button to dom
             boardDiv.appendChild(btn);
+
+            // Add button to array
+            boardArr[i-1][j-1] = btn;
         }
     }
 
-    return {boardDiv};
+    console.log(boardArr);
 
 })();
+
